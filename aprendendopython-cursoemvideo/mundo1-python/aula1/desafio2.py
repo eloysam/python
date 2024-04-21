11b25ad946a9dfc6211585e37b1ba9840d56c28d
Eloysa Ferreira Monteiro <114229411+eloysam@users.noreply.github.com>
2024-04-21 09:58:08 -0300

    alterações criadas

---
+++ aprendendopython-cursoemvideo/mundo1-python/aula1/desafio2.py
@@ -0,0 +1,5 @@
+dia = int(input('Qual é o dia do seu aniversário?'))
+mes = str(input('Qual é o mês do seu aniversário?'))
+ano = int(input('Qual é o ano do seu aniversário?'))
+
+print('Você nasceu no dia', dia , 'de' , mes , 'de' , ano, '. Correto?')
---
+++ aprendendopython-cursoemvideo/mundo1-python/aula1/desafio3.py
@@ -0,0 +1,6 @@
+# EXERCÍCIO 03 -----------------------------------------------------------------
+
+num1 = input('Entre com um número:')
+num2 = input('Entre com um segundo número:')
+
+print('A soma desses numeros é', num1 + num2)
