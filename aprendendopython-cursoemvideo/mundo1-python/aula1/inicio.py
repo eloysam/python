80278a18fb5c768ef568efd7ce277070ae16eed3
Eloysa Ferreira Monteiro <114229411+eloysam@users.noreply.github.com>
2024-04-21 09:52:14 -0300

    subindo os arquivos para o github

---
+++ aprendendopython-cursoemvideo/mundo1-python/aula1/inicio.py
@@ -0,0 +1,20 @@
+# Primeiro comando
+
+print('Olá Mundo!')
+print('O resultado é:',3 + 7)
+
+valor = '7' + '4'
+print(valor, type(valor))
+
+# Exemplo 1 -------------------------------------
+
+nome = input('Qual é o seu nome?')
+idade = input('Qual é a sua idade?')
+peso = input('Qual é o seu peso?')
+
+print(nome, idade, peso)
+
+# No python utiliza-se tanto a vírgula quanto o + para separar os valores no print. Percebemos a diferença no contexto que utilizmos
+# CLASS TYPE -> Retorna o tipo da variável.
+# Os numeros são mais utilizados para fazer cálculos
+# No Python as variáveis são objetos. (IMPORTANTE) -> Um objeto é mais do que uma variável
